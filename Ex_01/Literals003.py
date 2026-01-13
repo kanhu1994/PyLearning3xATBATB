bb3fb5512e687e21f79ac891eccc748621bc1939
kanhu <keshwarireddy@1995@gmail.com>
2026-01-14 01:37:56 +0530

    Adding Literals file

---
+++ Ex_01/Literals003.py
@@ -0,0 +1,13 @@
+import math
+
+a=10
+b=20
+print(a!=b)
+print(a>=b)
+print(not a)
+f=False
+t=True
+# Or Gate | And Gate
+print(f and t)
+print(f or t)
+print(math.pi*(float(input("Enter a number \n"))**2))
