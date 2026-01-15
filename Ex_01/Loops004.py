4b4d605816f0caf1d72d2b46a5f540987b52f328
kanhu <keshwarireddy@1995@gmail.com>
2026-01-16 00:52:09 +0530

    Adding Loops and if else file

---
+++ Ex_01/Loops004.py
@@ -0,0 +1,35 @@
+# # # # age = int(input("Enter your age : "))
+# # # # if age > 18:
+# # # #     print("you are eligible for vote")
+# # # # else:
+# # # #     print("Not eligible for vote")
+# # # # for i in range(1,5):
+# # # #     print(i)
+# # # i = 0
+# # # while i < 5:
+# # #     print(i)
+# # #     i = i + 1
+# # for i in range(10):
+# #     if i==5 or i==7:
+# #         pass
+# #     else:
+# #         print(i)
+# numbers=int(input("Enter a number : \n"))
+# match numbers:
+#     case 1:
+#         print("You have entered 1")
+#     case 2:
+#         print("You have entered 2")
+#     case 3:
+#         print("You have entered 3")
+#     case _:
+#         print("No match found")
+# User Defined Function
+def greet(name):
+    print("Hi kanhu",name)
+
+greet("kanhu")
+greet("sanu")
+greet(123345)
+
+
